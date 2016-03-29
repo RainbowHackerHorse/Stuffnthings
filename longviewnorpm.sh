ae2b91bb81383e50acd675f30ddab00455a7abd4
Rainbow <samantha@smsittech.com>
2016-03-29 00:44:52 -0400

    Update longviewnorpm.sh
    Now with new Quick And Dirty The Heck With It support!
    Now actually works!

--- longviewnorpm.sh
+++ longviewnorpm.sh
@@ -5,7 +5,20 @@
 # for license details
 
 set -e
-wget https://yum-longview.linode.com/centos/7/noarch/linode-longview-1.1-4.noarch.rpm
+if cat /etc/redhat-release | grep -q 7; then
+	echo "Detected CentOS 7"
+	wget https://yum-longview.linode.com/centos/7/noarch/linode-longview-1.1-4.noarch.rpm
+elif cat /etc/redhat-release | grep -q 6; then
+	echo "Detected CentOS 6"
+	wget https://yum-longview.linode.com/centos/6/noarch/linode-longview-1.1-4.noarch.rpm
+fi
+wget https://raw.githubusercontent.com/linode/longview/master/Extras/install-dependencies.sh
+sed -i.bak 's/\/tmp/\~/g' install-dependencies.sh 
+bash install-dependencies.sh
+#I'm just fumbling with perl deps at this point. jesus.
+# Next line because for some reason longview cant find half the stuff installed by install-dependancies.sh so whatever,
+# let's just install things globally and figure it out later. I'm a bad person for doing this ._.
+cpan install LWP::UserAgent Crypt::SSLeay IO::Socket::INET6 Linux::Distribution JSON::PP JSON Log::LogLite Try::Tiny DBI
 rpm2cpio linode-longview-1.1-4.noarch.rpm | cpio -idmv
 cd ./opt
 mv ./* /opt/

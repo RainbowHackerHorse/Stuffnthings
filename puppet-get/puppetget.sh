da498a82db7ec6b15efe65c991a06652001504cb
Rainbow <rainbow@hacker.horse>
2017-08-12 19:03:27 -0700

    Add verbosity

--- puppet-get/puppetget.sh
+++ puppet-get/puppetget.sh
@@ -10,15 +10,29 @@ PNUMODULENAME=$(echo "${PFILENAME}" | awk 'BEGIN{ FS = "-" } {print $2}')
 
 # Ensure creation of Module directory
 if [ ! -d "${GETDIR}" ] ;then
+	echo "Creating ${GETDIR}"
+	sleep 2
 	mkdir "${GETDIR}"
 fi
 
 cd "${GETDIR}" || exit
 
+#Download Tarball
+echo "Downloading ${PFILENAME}"
+sleep 2
 wget "${URLGET}"
 
 #Untar
+echo "Untarring to ${PMODULENAME}"
+sleep 2
 tar xvfz "${PFILENAME}"
 
 #Strip everything but the module name
+echo "Renaming ${PMODULENAME} to ${PNUMODULENAME}"
+sleep 2
 mv "${PMODULENAME}" "${PNUMODULENAME}"
+
+# Delete tarball
+echo "Deleting ${PFILENAME}"
+sleep 2
+rm "${GETDIR}"/"${PFILENAME}"

a7aa7ff35f5e7a14022456ecc30a3870c8cbe3f3
Rainbow <rainbow@hacker.horse>
2017-09-20 17:14:13 -0700

    Fix erroneous regex in cmd

--- misc-system-utils/mass-puppet-fix.sh
+++ misc-system-utils/mass-puppet-fix.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # mass run puppet-lint --fix
 # run from inside whatever directory is above your puppet modules dir.
-brokenmodules=$(puppet-lint ./modules | awk -F "-" '!/nagios/ {print $1}' | uniq)
+brokenmodules=$(puppet-lint ./modules | awk -F "-" '{print $1}' | uniq)
 for i in $brokenmodules; do
 	puppet-lint --fix "$i"
 done
